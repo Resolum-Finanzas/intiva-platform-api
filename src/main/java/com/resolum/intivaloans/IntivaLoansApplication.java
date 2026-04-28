2970604924b5e62c1a65b3cab890db9877d8affc
Nicolas Juarez <juarezleonn2000@gmail.com>
2026-04-27 22:55:17 -0500

    chore: initial commit.

---
+++ src/main/java/com/resolum/intivaloans/IntivaLoansApplication.java
@@ -0,0 +1,13 @@
+package com.resolum.intivaloans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntivaLoansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntivaLoansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resolum/intivaloans/IntivaLoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolum.intivaloans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntivaLoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
